d302cdfd6a864b22665c24bcde34e06ec5c5d752
Marat Fakhreev <marat.fakhreev@flatstack.com>
2017-09-05 14:02:49 +0300

    Update blueprint entry point

--- blueprint/src/index.js
+++ blueprint/src/index.js
@@ -1,3 +1,7 @@
-require.context('views', false, /\.pug$/);
-require.context('stylesheets', false, /\.css$/);
-require.context('scripts', false, /\.js/);
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
+requireAll(require.context('views', false, /\.pug$/));
+requireAll(require.context('stylesheets', false, /\.css$/));
+requireAll(require.context('scripts', false, /\.js/));
